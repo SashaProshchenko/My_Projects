1538999ce1b31cbc6157779108c0cf49af6d85a1
SashaProshchenko <sasha.proshchenko@gmail.com>
2020-04-12 21:01:24 +0300

    v1.0.1 little fixes

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ml_articles.db'
+app.secret_key = "flask rocks!"
 
 db = SQLAlchemy(app)
--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from wtforms import Form, StringField, SelectField
 
 class ArticleSearchForm(Form):
-    choices = [('Article name', 'Article Name'),
+    choices = [('Article Name', 'Article Name'),
                ('Release Year', 'Release Year'),
                ('Publisher', 'Publisher')]
     select = SelectField('Search article:', choices=choices)
@@ -10,7 +10,7 @@ class ArticleSearchForm(Form):
 
 class ArticleForm(Form):
 
-    article = StringField('Article name')
+    article = StringField('Article Name')
     url = StringField('URL')
     release_date = StringField('Release Year')
     publisher = StringField('Publisher')
